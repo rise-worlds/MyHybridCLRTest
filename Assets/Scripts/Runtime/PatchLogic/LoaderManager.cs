7cb695b48b7d6280eb27deffb8cecfe087d0a73a
rise <rise.worlds@outlook.com>
2026-04-20 17:47:36 +0800

    backup

---
+++ Assets/Scripts/Runtime/PatchLogic/LoaderManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using Core;
+using Core.Singleton;
+using UnityEngine;
+using UniFramework.Event;
+using YooAsset;
+
+public class LoaderManager: Singleton<LoaderManager>
+{
+    /// <summary>
+    /// 协程启动器
+    /// </summary>
+    public MonoBehaviour Behaviour;
+
+    /// <summary>
+    /// 开启一个协程
+    /// </summary>
+    public void StartCoroutine(IEnumerator enumerator)
+    {
+        Behaviour.StartCoroutine(enumerator);
+    }
+
+}
